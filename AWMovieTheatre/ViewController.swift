7d14c395334ee2bcc9e80836e1fd3561cdd40bf9
Aaron.Wolfe <3wolfeaaron5@gmail.com>
2020-04-02 14:20:15 -0500

    Adds remaining UI elements with constraints and outlets

--- AWMovieTheatre/ViewController.swift
+++ AWMovieTheatre/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 }
 
